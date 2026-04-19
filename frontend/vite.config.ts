75282885b527a09d64e38627313af889c39c6ec8
Kamil <kamil.p@tutanota.com>
2026-04-19 20:22:26 +0200

    Add checkout page with multi-step form. Add setting for deploying on github

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,18 +3,19 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-    dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime"],
-  },
+	base: "/fullstack-force/",
+	server: {
+		host: "::",
+		port: 8080,
+		hmr: {
+			overlay: false,
+		},
+	},
+	plugins: [react()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+		dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime"],
+	},
 });
